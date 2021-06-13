6e2d5638d3a4e1054ad6ee61298e85f222b371d7
valladao <valladao@gmail.com>
2021-06-13 08:50:56 -0300

    Add an expense

--- server.js
+++ server.js
@@ -2,17 +2,72 @@ const express = require("express")
 const mongo = require("mongodb").MongoClient
 
 const app = express()
+const url =
+  "mongodb://mongo:LfVl0jUlG26nskrbQrZh@containers-us-west-7.railway.app:7917"
+
+app.use(express.json())
+
+let db, trips, expenses
+
+mongo.connect(
+  url,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err, client) => {
+    if (err) {
+      console.error(err)
+      return
+    }
+    db = client.db("tripcost")
+    trips = db.collection("trips")
+    expenses = db.collection("expenses")
+  }
+)
 
 app.post("/trip", (req, res) => {
-  /* */
+  const name = req.body.name
+  trips.insertOne({ name: name }, (err, result) => {
+    if (err) {
+      console.error(err)
+      res.status(500).json({ err: err })
+      return
+    }
+    console.log(result)
+    res.status(200).json({ ok: true })
+  })
 })
 
 app.get("/trips", (req, res) => {
-  /* */
+  trips.find().toArray((err, items) => {
+    if (err) {
+      console.error(err)
+      res.status(500).json({ err: err })
+      return
+    }
+    res.status(200).json({ trips: items })
+  })
 })
 
 app.post("/expense", (req, res) => {
-  /* */
+  expenses.insertOne(
+    {
+      trip: req.body.trip,
+      date: req.body.date,
+      amount: req.body.amount,
+      category: req.body.category,
+      description: req.body.description,
+    },
+    (err, result) => {
+      if (err) {
+        console.error(err)
+        res.status(500).json({ err: err })
+        return
+      }
+      res.status(200).json({ ok: true })
+    }
+  )
 })
 
 app.get("/expenses", (req, res) => {
