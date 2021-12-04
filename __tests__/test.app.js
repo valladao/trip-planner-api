29856e75d6e7ca059418eea929ecd4d67cc30d2a
Manoel Valladão <valladao@gmail.com>
2021-12-04 14:54:51 -0300

    Using supertest

--- __tests__/test.app.js
+++ __tests__/test.app.js
@@ -1,4 +1,5 @@
 const app = require("../server")
+const request = require("supertest")
 
 beforeAll(function (done) {
   app.on("appStarted", done)
@@ -6,4 +7,18 @@ beforeAll(function (done) {
 
 afterAll(() => {
   app.emit("closeApp")
+})
+
+describe("Test trips API", () => {
+  it("should create a new trip", (done) => {
+    request(app)
+      .post("/trip")
+      .send({
+        name: "Toring the Alps",
+      })
+      .set("Content-Type", "application/json")
+      .set("Accept", "application/json")
+      .expect(200)
+      .end(done)
+  })
 })
